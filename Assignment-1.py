a3f5976f1f8407f9aa04c66b96c6fe0d575047b9
Deeraj Anandraj <deeraj0830@gmail.com>
2026-02-08 22:49:13 -0500

    feat: added the process_coints function

--- Assignment-1.py
+++ Assignment-1.py
@@ -54,7 +54,15 @@ class SandwichMachine:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-
+        print("Please insert coins.")
+        large_dollars = int(input("how many large dollars?: "))
+        half_dollars = int(input("how many half dollars?: "))
+        quarters = int(input("how many quarters?: "))
+        nickels = int(input("how many nickels?: "))
+        
+        total = (large_dollars * 1.0) + (half_dollars * 0.5) + (quarters * 0.25) + (nickels * 0.05)
+        return total
+    
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
